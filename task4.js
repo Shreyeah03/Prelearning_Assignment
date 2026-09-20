40f5c001091e8e92dd3267c8c6f27cf38f05424d
Shreyeah03 <shreyakhacharia305@gmail.com>
2026-09-20 15:47:51 +0530

    task4 completed

---
+++ task4.js
@@ -0,0 +1,37 @@
+/*Task 4: Write a JavaScript program that creates a class called University with properties 
+for university names and departments. Include methods to add a department, remove a 
+department, and display all departments. Create an instance of the University class and 
+add and remove departments. */
+
+
+
+
+class University {
+    constructor(name) {
+        this.name = name;
+        this.departments = [];
+    }     
+      
+    addDepartment(department) {
+        this.departments.push(department);
+    }
+
+    removeDepartment(department) {      
+      this.departments=this.departments.filter(dep=>dep!==department);
+    }
+
+    displayDepartments(){
+        console.log("Departments in " + this.name +" are:");
+        for(let i=0;i<this.departments.length;i++){
+            console.log(this.departments[i]);
+        }
+    }
+}
+
+const university = new University("CVM University");
+
+university.addDepartment("Computer Science");
+university.addDepartment("Chemical Engineering");
+university.addDepartment("Mechanical Engineering");
+university.removeDepartment("Electrical Engineering");
+university.displayDepartments();
