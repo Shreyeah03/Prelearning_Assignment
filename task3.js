1238df05fbc118b5512fc1da02b9e5aa4312789f
Shreyeah03 <shreyakhacharia305@gmail.com>
2026-09-20 15:38:10 +0530

    completed task 3

---
+++ task3.js
@@ -0,0 +1,46 @@
+/*Task 3: Write a JavaScript program that creates a class called 'Shape' with a method to 
+calculate the area. Create two subclasses, 'Circle' and 'Triangle', that inherit from the 
+'Shape' class and override the area calculation method. Create an instance of the 'Circle' 
+class and calculate its area. Similarly, do the same for the 'Triangle' class. */
+
+class Shape {
+    area() {
+        return 0;
+    }   
+    
+}
+
+class Circle extends Shape {
+    constructor(radius) {
+        super();
+        this.radius = radius;
+    }
+
+
+    area() {
+        return Math.PI * this.radius ** 2;
+    }
+}
+
+class Triangle extends Shape {
+    constructor(base, height) {
+        super();
+        this.base = base;
+        this.height = height;
+    }
+
+    area() {
+        return 0.5 * this.base * this.height;
+    }
+}
+
+
+const circle = new Circle(5);
+console.log("Circle's area is:", circle.area());
+
+const triangle = new Triangle(10, 8);
+console.log("Triangle's area is:", triangle.area());         
+
+
+
+
